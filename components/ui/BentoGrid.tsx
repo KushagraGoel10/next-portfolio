819e3bb38f8354ec7157f1bcd592e142a41b34f7
kushagra <goelkushagra2000@gmail.com>
2024-09-02 19:28:05 +0530

    updated experience, skills

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "Express", "Javascript"];
+  const rightLists = ["NextJs", "NodeJs", "GraphQL"];
 
   const [copied, setCopied] = useState(false);
 
